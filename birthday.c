df9d2bfe74d2ce3b5d32a3f44dc9eea6da2a1ef0
Prashant19111 <71460135+Prashant19111@users.noreply.github.com>
2021-01-30 11:22:58 +0530

    Update birthday.c

--- birthday.c
+++ birthday.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <windows.h>
 
 int main()
 {
@@ -10,33 +11,32 @@ int main()
      struct tm *loc=localtime(&now);
      d=(*loc).tm_mday;
      m=(*loc).tm_mon+1;
-     char s[10],s1[20],s2[20];
+     char s[10],s1[2];
      sprintf(s,"%02d-%02d",d,m);
      
      FILE *ptr;
-     ptr=fopen("birthdate.txt","r");
+     ptr=fopen("D:/birthday-reminder-master/birthdate.txt","r");
     
-     int i=strlen(s);
+    
+    int i=1,flag=0,count=1;
+    char name[50],date[10];
+    char buf[100];
+    char *c;
+    do{
+        if(i%2!=0){
+            fgets(name,50,ptr);
+        }
+        if(i%2==0){
+            fgets(date,6,ptr);
+            if(!strcmp(s,date)){
+                snprintf(buf,100,"Today Is Your Friend %s's Birthday",name);
+                MessageBox(0,buf,"Today's Birthday",64);
+                count++;
+            }
+            fgets(date,2,ptr);
+        }
+        i++;
+    }while(!feof(ptr));
      
-     while(!feof(ptr))
-     {
-         fgets(s1,i+1,ptr);
-         int x=strcmp(s1,s);
-         if(x>0||x<0)
-         {
-             strcpy(s2,s1);
-             fgets(s1,10,ptr);
-             strcat(s2,s1);
-         }
-         
-         if(x==0)
-         {
-             printf("Todat is %s",s2);
-             printf(" birthday");
-             break;
-         }
-     }
-     
-
     return 0;
 }
